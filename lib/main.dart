101a14ac12f83f5f0ef0e820fba947eb92570c91
nargyanti <nargyanti@gmail.com>
2021-10-30 00:22:02 +0700

    [Jobsheet 4] Practicum

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,23 @@ class _MyAppState extends State<MyApp> {
   TextEditingController inputController = new TextEditingController();
 
   double _inputUser = 0;
-  double _kelvin = 0;
-  double _reamur = 0;
-  double _fahrenheit = 0;
+  String _newValue = "Kelvin";
+  double _result = 0;
+
+  var listItem = ["Kelvin", "Reamur", "Fahrenheit"];
 
   void convertTemperature() {
     setState(() {
       _inputUser = double.parse(inputController.text);
-      _kelvin = _inputUser + 273;
-      _reamur = 4 / 5 * _inputUser;
-      _fahrenheit = 9 / 5 * _inputUser + 32;
-    });
-    print('Done');
+
+      if (_newValue == 'Kelvin') {
+        _result = _inputUser + 273;
+      } else if (_newValue == 'Reamur') {
+        _result = 4 / 5 * _inputUser;
+      } else if (_newValue == 'Fahrenheit') {
+        _result = 9 / 5 * _inputUser + 32;
+      }
+    });    
   }
 
   @override
@@ -49,12 +54,25 @@ class _MyAppState extends State<MyApp> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Input(inputController),
+              DropdownButton<String>(
+                items: listItem.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                value: _newValue,
+                onChanged: (changeValue) {
+                  setState(() {
+                    _newValue = changeValue.toString();
+                    _inputUser = double.parse(inputController.text);
+                  });
+                },
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Result("Kelvin", _kelvin),
-                  Result("Reamur", _reamur),
-                  Result("Fahrenheit", _fahrenheit),
+                  Result(_result),                  
                 ],
               ),
               Container(child: Convert(convertTemperature)),
--- lib/result.dart
+++ lib/result.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Result extends StatelessWidget {
-  final String title;
-  final double temperature;
+class Result extends StatelessWidget {  
+  final double result;
 
-  Result(this.title, this.temperature);
+  Result(this.result);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          title,
-          style: TextStyle(fontSize: 20),
-        ),
-        Text(
-          temperature.toStringAsFixed(2),
-          style: TextStyle(fontSize: 30),
-        ),
-      ],
+    return Container(
+      margin: EdgeInsets.only(top: 20, bottom: 20),      
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Hasil',
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            result.toStringAsFixed(2),
+            style: TextStyle(fontSize: 30),
+          ),
+        ],
+      ),
     );
   }
 }
